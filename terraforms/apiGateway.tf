aa4f0edf07badeb8b74a319510c928ddcd9f9cc5
SoudagarLondheRIT <srl1622@rit.edu>
2024-03-31 11:41:42 -0400

    API for invoke student authentication Lambda function

--- terraforms/apiGateway.tf
+++ terraforms/apiGateway.tf
@@ -445,3 +445,133 @@ resource "aws_api_gateway_integration_response" "api2_options_filename_integrati
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
+
+
+# ------------------------------------------------------------------------------
+# 3rd API : 
+
+# Create resource for /studentidentify
+resource "aws_api_gateway_resource" "student_identify_resource" {
+  rest_api_id = aws_api_gateway_rest_api.ams_apis_tf.id
+  parent_id   = aws_api_gateway_rest_api.ams_apis_tf.root_resource_id
+  path_part   = "studentidentify"
+}
+
+# Create GET method for /studentidentify
+resource "aws_api_gateway_method" "get_student_identify_method" {
+  rest_api_id = aws_api_gateway_rest_api.ams_apis_tf.id
+  resource_id = aws_api_gateway_resource.student_identify_resource.id
+  http_method = "GET"
+  authorization = "NONE" 
+}
+
+# Integration with Lambda function for /studentidentify GET method
+resource "aws_api_gateway_integration" "student_identify_integration" {
+  rest_api_id = aws_api_gateway_rest_api.ams_apis_tf.id
+  resource_id = aws_api_gateway_resource.student_identify_resource.id
+  http_method = aws_api_gateway_method.get_student_identify_method.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY" 
+  # Change the following
+   uri   = aws_lambda_function.student_registration.invoke_arn
+  # uri   = aws_lambda_function.terraform_lambda_func_authentication.invoke_arn
+
+  # credentials = aws_iam_role.lambda_role.arn
+}
+
+# resource "aws_lambda_permission" "apigw_lambda" {
+#   statement_id  = "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.terraform_lambda_func_authentication.function_name
+#   principal     = "apigateway.amazonaws.com"
+#   # source_arn    = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.example_api.id}/*/${aws_api_gateway_method.example_method.http_method}${aws_api_gateway_resource.example_resource.path}"
+#   # source_arn = "${aws_api_gateway_rest_api.ams_apis_tf.execution_arn}/*"
+
+#   # source_arn    = "arn:aws:execute-api:us-east-1:905418464683:5zwtmpd2f2/*/GET/studentidentify"
+#   source_arn = 
+#   # aws_s3_bucket.new_student_registration_tf.arn
+ 
+# }
+
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+ 
+  #Change the following
+  function_name = aws_lambda_function.student_registration.arn
+  # function_name = aws_lambda_function.terraform_lambda_func_authentication.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.ams_apis_tf.execution_arn}/*/*"
+}
+
+# Method Response for /studentidentify GET method
+resource "aws_api_gateway_method_response" "student_identify_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.ams_apis_tf.id
+  resource_id = aws_api_gateway_resource.student_identify_resource.id
+  http_method = aws_api_gateway_method.get_student_identify_method.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+# Options Method for /studentidentify
+resource "aws_api_gateway_method" "options_student_identify_method" {
+  rest_api_id   = aws_api_gateway_rest_api.ams_apis_tf.id
+  resource_id   = aws_api_gateway_resource.student_identify_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE" 
+}
+
+# Integration for Options Method for /studentidentify
+resource "aws_api_gateway_integration" "options_student_identify_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.ams_apis_tf.id
+  resource_id             = aws_api_gateway_resource.student_identify_resource.id
+  http_method             = aws_api_gateway_method.options_student_identify_method.http_method
+  integration_http_method = "OPTIONS"
+  type                    = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+# Method Response for Options Method for /studentidentify
+resource "aws_api_gateway_method_response" "options_student_identify_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.ams_apis_tf.id
+  resource_id = aws_api_gateway_resource.student_identify_resource.id
+  http_method = aws_api_gateway_method.options_student_identify_method.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+
+# Integration Response for Options Method for /studentidentify
+resource "aws_api_gateway_integration_response" "options_student_identify_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.ams_apis_tf.id
+  resource_id = aws_api_gateway_resource.student_identify_resource.id
+  http_method = aws_api_gateway_method.options_student_identify_method.http_method
+  status_code = aws_api_gateway_method_response.options_student_identify_method_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'", 
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
