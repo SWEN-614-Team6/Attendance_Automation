02d04570e625de17b0c3d45a7a2dc76b0d989b65
Atharva Chiplunkar <73574922+AtharvaChiplunkar12@users.noreply.github.com>
2024-04-23 12:59:51 -0400

    removed unnecessary imports and code comments

--- terraforms/s3.tf
+++ terraforms/s3.tf
@@ -3,15 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-  locals {
-  relative_path_to_external_directory = "dataset"
-#   dataset_path = "${path.root}/${local.relative_path_to_external_directory}"
-  dataset_path = replace(path.cwd,"terraforms","dataset")
-}
-
-
-
 #Creating s3 bucket to upload the student image in a s3 bucket.
 resource "aws_s3_bucket" "new-student-registration-tf" {
   bucket = "new-student-registration-tf"
@@ -34,16 +25,5 @@ resource "aws_s3_bucket" "class-images-tf" {
   }
 }
 
-data "local_file" "ses_email" {
-  filename = replace(path.cwd,"terraforms","SES_EMAIL.txt")
-}
 
-locals {
-  email_line = data.local_file.ses_email.content
-  email_id   = trim(split("=", local.email_line)[1], " ")
-}
-
-output "email_id" {
-  value = local.email_id
-}
 
